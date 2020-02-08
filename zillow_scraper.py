a0d161713355c0b613ac36daa7d68901c87a1beb
Ray Salem <raysalem@gmail.com>
2020-02-07 16:07:43 -0800

    Create zillow_scraper.py

---
+++ zillow_scraper.py
@@ -0,0 +1,11 @@
+# Zillow scraper
+
+[![zillow](http://www.zillow.com/widgets/GetVersionedResource.htm?path=/static/logos/Zillowlogo_200x50.gif)](http://www.zillow.com)
+
+## note
+### branding requirement
+https://www.zillow.com/howto/api/BrandingRequirements.htm#zillowlogo
+
+
+
+ZWSID = "X1-ZWz1hm3iutj85n_7yjzz"
